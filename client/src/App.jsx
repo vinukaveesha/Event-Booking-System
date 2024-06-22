fca52e1963c8caf7fcb3dd3272f50466268e3b00
Dasuni <vinukaveesha2001@gmail.com>
2024-06-22 23:33:24 +0530

    form tracker navigation added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,15 +8,21 @@ import Welcome from './Pages/PageWelcome';
 import DefaultLayout from './Layouts/DefaultLayout';
 import DashboardWelfare from './Pages/Dashboard/DashboardWelfare';
 import AfterSubmit from './Pages/PageAfterSubmitt';
+import VenueBooking from './Pages/PageVenueBooking';
+import PendingFormsEntry from './Pages/PendingForms/PagePendingForms.entry';
+import PendingForms from './Pages/PendingForms/PagePendingForms';
 
 function App() {
   return (
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome/>} />
+        <Route path="/" element={<PendingForms/>} />
         <Route path="/login-page" element={<Login />} />
         <Route path="/after-submit" element={<AfterSubmit />} />
+        <Route path="/venue-booking" element={<VenueBooking />} />
+        <Route path="/pending-forms-entry" element={<PendingFormsEntry />} />
         <Route path="/event-form" element={<EventForm />} />
+        <Route path="/pending-forms" element={<PendingForms />} />
       </Routes>
 
       </BrowserRouter>
