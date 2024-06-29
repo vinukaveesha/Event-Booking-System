ca713ee3e97f96938c81f226317b0b06a372f11e
Dasuni <vinukaveesha2001@gmail.com>
2024-06-29 15:34:57 +0530

    basic structure of server side added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, useNavigate, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter , Route, Routes, Router,Switch} from 'react-router-dom';
 import './App.css';
 
 import Login from './Pages/Login';
@@ -7,16 +7,21 @@ import EventForm from './Pages/PageEventForm';
 import Welcome from './Pages/PageWelcome';
 import DefaultLayout from './Layouts/DefaultLayout';
 import DashboardWelfare from './Pages/Dashboard/DashboardWelfare';
+import DashboardDVC from './Pages/Dashboard/DashBoardDVC';
 import AfterSubmit from './Pages/PageAfterSubmitt';
 import VenueBooking from './Pages/PageVenueBooking';
 import PendingFormsEntry from './Pages/PendingForms/PagePendingForms.entry';
 import PendingForms from './Pages/PendingForms/PagePendingForms';
 
+
 function App() {
+
   return (
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PendingForms/>} />
+        <Route path="/" element={<Welcome/>} />
+        <Route path="/dashboard-welfare" element={<DashboardWelfare />} />
+        <Route path="/dashboard-dvc" element={<DashboardDVC />} />
         <Route path="/login-page" element={<Login />} />
         <Route path="/after-submit" element={<AfterSubmit />} />
         <Route path="/venue-booking" element={<VenueBooking />} />
@@ -24,10 +29,10 @@ function App() {
         <Route path="/event-form" element={<EventForm />} />
         <Route path="/pending-forms" element={<PendingForms />} />
       </Routes>
-
       </BrowserRouter>
 
   );
 }
 
 export default App;
+
